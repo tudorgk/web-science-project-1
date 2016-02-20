3ba34c77df3742ffddb00d0be739370b9f4c0732
Tudor Dragan <tudorgk@gmail.com>
2016-02-20 17:59:14 +0100

    some refactoring

--- src/FileSanitizer.py
+++ src/FileSanitizer.py
@@ -10,11 +10,14 @@ import string
 import re
 
 
-def analyzer(inputfile):
-    review=inputfile.read()
-    tokens=[e.lower() for e in map(string.strip, re.split("(\W+)", review)) if len(e) > 0 and not re.match("\W",e)]
+def tokenize_file(input_file):
+    review = input_file.read()
+    tokens = [e.lower()
+              for e in
+              map(string.strip, re.split("(\W+)", review))
+              if len(e) > 0 and not re.match("\W",e)]
 
-    print tokens
+    return tokens
 
 
 def main(arguments):
@@ -31,7 +34,9 @@ def main(arguments):
     args = parser.parse_args()
     for f in args.file:
         # print f
-        analyzer(f)
+        tokenized_words = tokenize_file(f)
+        print tokenized_words
+
 
 if __name__ == '__main__':
     sys.exit(main(sys.argv[1:]))
